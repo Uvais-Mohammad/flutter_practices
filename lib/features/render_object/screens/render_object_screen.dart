6d8dc83c8c9849b1908a7e4402d9d056874b6a79
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-10-25 23:33:27 +0400

    feat:render objects with custom paints

---
+++ lib/features/render_object/screens/render_object_screen.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+import 'package:aqary_test/features/render_object/widgets/render_custom_object.dart';
+import 'package:flutter/material.dart';
+
+class RenderObjectScreen extends StatelessWidget {
+  const RenderObjectScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Render Object Screen'),
+      ),
+      body: Center(
+        child: CustomBox(
+          flex: 1,
+          color: Colors.red,
+          rotation: pi / 3,
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
